66f47734384818abb0548558aba8cac01e80a47f
SMerilles-Rosmini <21098@rosmini.school.nz>
2026-02-25 19:47:43 +1300

    got the products page link to work but the content disappeared

--- app.py
+++ app.py
@@ -28,21 +28,21 @@ def query_db(query, args=(), one=False):
 @app.route('/')
 def home():
     #home page - ID, manufacturer, image URL
-    sql = """SELECT Products.product_id, manufacturers.name, Products.image_url 
+    sql = """SELECT Products.product_name, Products.product_id, manufacturers.name, Products.image_url 
             FROM Products
-            JOIN manufacturers ON manufacturers.manufacturer_id = Products.product_id;"""
+            JOIN manufacturers ON manufacturers.manufacturer_id = Products.manufacturer_id;"""
     results = query_db(sql)
-    return render_template("templates\products.html", results=results)
+    return render_template("layout.html", results=results)
     # return str(results)
 
 @app.route("/products/Violins")
-def product(id):
+def product():
     # just one product
     sql = """SELECT * FROM Products 
         WHERE Products.product_type = 'Violin';"""
-    result = query_db(sql, (id,), True)
-    # return render_template("products.html", result=result)
-    return str(result)
+    result = query_db(sql)
+    return render_template("products.html", result=result)
+    # return str(result)
 
 
 if __name__ == '__main__':
